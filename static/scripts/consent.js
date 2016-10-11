eea0571c242088a48a425e9c1d8dc2d813add3bb
thomasmorgan <thomas.j.h.morgan@gmail.com>
2016-10-11 15:34:07 -0700

    tidy up js

--- static/scripts/consent.js
+++ static/scripts/consent.js
@@ -12,4 +12,6 @@ $(document).ready(function() {
         allow_exit();
         self.close();
     });
+
+    create_participant();
 });
--- static/scripts/experiment.js
+++ static/scripts/experiment.js
@@ -1,27 +1,7 @@
-var tiles_checked = 0;
-var decided = false;
-var trials_per_network;
-var trial_in_this_network = 0;
-var round_number = 0;
-var number_of_rounds;
-var available_bandit_names = ["Afghanistan", "Albania", "Argentina", "Australia", "Austria", "Bangladesh", "Belgium", "Botswana", "Brasil", "Bulgaria", "Burundi", "Canada", "Chad", "Chile", "China", "Colombia", "Costa Rica", "Croatia", "Denmark", "Ecuador", "Egypt", "England", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Germany", "Ghana", "Greece", "Greenland", "Guatemala", "Holland", "India", "Iran", "Ireland", "Italy", "Japan", "Laos", "Libya", "Madagascar", "Mali", "Mexico", "Mongolia", "Morocco", "Mozambique", "Myanmar", "Nepal", "New Zealand", "Nigeria", "Norway", "Pakistan", "Papua New Guinea", "Poland", "Portugal", "Romania", "Russia", "Scotland", "Senegal", "Sierra Leone", "South Korea", "Spain", "Sri Lanka", "Sweden", "Thailand", "The United States", "Tonga", "Tunisia", "Turkey", "Turkmenistan", "Ukraine", "Wales", "Yemen"];
-var bandit_names;
-var lock = false;
-var p_change = 0.4;
-bandit_mapping = [];
+$(document).ready(function() {
+    create_agent();
+});
 
-// get all the details to correctly present the trial number bar
-get_num_trials = function() {
-    reqwest({
-        url: "/num_trials",
-        method: 'get',
-        success: function (resp) {
-            trials_per_network = resp.n_trials;
-            number_of_rounds = resp.practice_repeats + resp.experiment_repeats;
-            prepare_trial_info_text();
-        }
-    });
-};
 
 // make a new node
 create_agent = function() {
@@ -36,13 +16,6 @@ create_agent = function() {
         },
         error: function (err) {
             console.log(err);
-            err_response = JSON.parse(err.response);
-            if (err_response.hasOwnProperty('html')) {
-                $('body').html(err_response.html);
-            } else {
-                allow_exit();
-                window.location = "/debriefing/debrief-1?participant_id=" + participant_id;
-            }
         }
     });
 };
