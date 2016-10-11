5c174be15324d0e1232cece53641702d8500cab6
thomasmorgan <thomas.j.h.morgan@gmail.com>
2016-10-11 14:51:56 -0700

    fix instructions 5

---
+++ static/scripts/instructions/instruct-5.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#begin-button").click(function() {
+        allow_exit();
+        go_to_page("experiment");
+    });
+    $("#prev-button").click(function() {
+        allow_exit();
+        go_to_page("instructions/instruct-4");
+    });
+});
