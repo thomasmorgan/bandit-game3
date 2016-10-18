f7ae441f392e0630bd925d02d1ac3cc3412e43cc
thomasmorgan <thomas.j.h.morgan@gmail.com>
2016-10-18 11:02:03 -0700

    instructions update

--- static/scripts/instructions/instruct-3.js
+++ static/scripts/instructions/instruct-3.js
@@ -7,4 +7,28 @@ $(document).ready(function() {
         allow_exit();
         go_to_page("instructions/instruct-2");
     });
+    reqwest({
+        url: "/experiment/trials_per_round",
+        method: 'get',
+        type: 'json',
+        success: function (resp) {
+            trials_per_round = resp.trials_per_round;
+            $(".trials").html(trials_per_round);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+    reqwest({
+        url: "/experiment/rounds",
+        method: 'get',
+        type: 'json',
+        success: function (resp) {
+            rounds = resp.rounds;
+            $(".rounds").html(rounds);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
 });
