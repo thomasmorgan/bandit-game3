82d8f94c4a715c2d391867c509f8b6d2f76f21a3
thomasmorgan <thomas.j.h.morgan@gmail.com>
2016-12-11 18:12:55 -0700

    disable the consent button until a participant object has been created

--- static/scripts/consent.js
+++ static/scripts/consent.js
@@ -1,4 +1,6 @@
 $(document).ready(function() {
+    $("#consent").prop('disabled', true);
+
     $("#print-consent").click(function() {
         window.print();
     });
@@ -15,3 +17,39 @@ $(document).ready(function() {
 
     create_participant();
 });
+
+// make a new participant
+create_participant = function() {
+
+    // check if the local store is available, and if so, use it.
+    if (typeof store != "undefined") {
+        url = "/participant/" +
+            store.get("worker_id") + "/" +
+            store.get("hit_id") + "/" +
+            store.get("assignment_id") + "/" +
+            store.get("mode");
+    } else {
+        url = "/participant/" +
+            worker_id + "/" +
+            hit_id + "/" +
+            assignment_id + "/" +
+            mode;
+    }
+
+    if (participant_id === undefined || participant_id === "undefined") {
+        reqwest({
+            url: url,
+            method: "post",
+            type: "json",
+            success: function(resp) {
+                console.log(resp);
+                participant_id = resp.participant.id;
+                $("#consent").prop('disabled', false);
+            },
+            error: function (err) {
+                errorResponse = JSON.parse(err.response);
+                $("body").html(errorResponse.html);
+            }
+        });
+    }
+};
